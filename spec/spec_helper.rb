23bc430787be37761c4f546c5fd8f6d74017131a
Corey Powell <mistdragon100@gmail.com>
2015-05-02 11:01:00 -0500

    Updated spec_helper

---
+++ spec/spec_helper.rb
@@ -0,0 +1,10 @@
+Encoding.default_internal = Encoding.default_external = 'UTF-8'
+
+require 'codeclimate-test-reporter'
+require 'simplecov'
+require 'active_support/core_ext/string'
+
+CodeClimate::TestReporter.start
+SimpleCov.start
+
+require 'moon-serializable/load'
