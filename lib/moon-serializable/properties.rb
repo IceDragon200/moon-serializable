ba0959ac6934430c3135868acd778be00d6dbdb4
Corey Powell <mistdragon100@gmail.com>
2015-05-14 09:25:09 -0500

    Updated Properties
    I think I should move this to its own package though, Moon::Accessible, probably?

--- lib/moon-serializable/properties.rb
+++ lib/moon-serializable/properties.rb
@@ -5,6 +5,8 @@ module Moon
     # Properties are special attributes on an object, its default implementation,
     # is to use instance variables, to change this overwrites the
     # #property_get and #property_set methods.
+    # When using Properties AND Serializable, be sure to include it BEFORE
+    # Serializable, though the order shouldn't matter too much.
     module Properties
       module ClassMethods
         extend Moon::Prototype
@@ -46,47 +48,65 @@ module Moon
       module InstanceMethods
         # @param [Symbol] key
         def property_get(key)
-          instance_variable_get "@#{key}"
+          public_send key
         end
 
         # @param [Symbol] key
         # @param [Object] value
         def property_set(key, value)
-          instance_variable_set "@#{key}", value
+          public_send "#{key}=", value
+        end
+
+        # @yieldparam [Symbol] key
+        def each_property_key
+          return to_enum :each_property_key unless block_given?
+          self.class.each_property.each do |key|
+            yield key
+          end
         end
 
         # @yieldparam [Symbol] key
         # @yieldparam [Object] value
-        def each_property
-          return to_enum :each_property unless block_given?
-          self.class.all_properties.each do |key|
+        def each_property_pair
+          return to_enum :each_property_pair unless block_given?
+          each_property_key do |key|
             yield key, property_get(key)
           end
         end
 
+        alias :each_property :each_property_pair
+
+        # @yieldparam [Symbol] key
+        # @yieldparam [Object] value
+        # @yieldreturn [Object] new_value
         def map_properties
           each_property do |key, value|
             property_set key, yield(key, value)
           end
         end
 
-        def to_h
+        # @return [Hash<Symbol, Object>]
+        def properties_to_h
           # This may be a bit slower than creating a result hash and setting
           # each key from the property.
-          each_property.to_a.to_h
+          each_property.to_h
         end
 
-        def serialization_properties(&block)
+        # (see #each_property)
+        def exported_properties(&block)
           each_property(&block)
         end
 
-        def serializable_inspect
-          ptr = format('%x', __id__)
-          result = "<#{self.class}#0x#{ptr}: "
-          each_property do |key, value|
+        # @return [String]
+        def inspect_properties
+          id = format('%08x', __id__)
+          result = "<#{self.class}#0x#{id}: "
+          each_property_pair do |key, value|
             s = case value
-            when Hash then '{...}'
-            when Array then '[...]'
+            when Hash
+              "#{value.empty? ? '{}' : '{...}'}[#{value.size}]"
+            when Array
+              "#{value.empty? ? '[]' : '[...]'}[#{value.size}]"
             else
               value.inspect
             end
@@ -96,8 +116,9 @@ module Moon
           result
         end
 
+        # @return [String]
         def inspect
-          serializable_inspect
+          inspect_properties
         end
       end
 
